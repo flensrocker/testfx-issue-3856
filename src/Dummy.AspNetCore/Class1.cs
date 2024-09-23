1da8f5c558a7ee459b10397bfd8140f979714bd0
Lars Hanisch <dvb@flensrocker.de>
2024-09-23 16:11:29 +0200

    Reproduction Sample for microsoft/testfx#3856

---
+++ src/Dummy.AspNetCore/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Dummy.AspNetCore;
+
+public class Class1
+{
+  public void DummyMethod(Microsoft.AspNetCore.Authentication.AuthenticationScheme authScheme)
+  {
+    if (string.IsNullOrEmpty(authScheme.Name))
+      throw new Exception("Dummy");
+  }
+}
---
+++ test/Dummy.UnitTests/Test1.cs
@@ -0,0 +1,17 @@
+﻿using Dummy.AspNetCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dummy.UnitTests;
+
+[TestClass]
+public sealed class Test1
+{
+  [TestMethod]
+  public void TestMethod1()
+  {
+    var authScheme = new Microsoft.AspNetCore.Authentication.AuthenticationScheme("Name", "DisplayName", typeof(string));
+
+    var dummy = new Class1();
+    dummy.DummyMethod(authScheme);
+  }
+}
